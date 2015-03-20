a7fd91ab0e5e92af9e94d67e0ae4a508313e7bd2
ChrisDujmic <chrisdujmic@gmail.com>
2015-03-20 13:31:43 +0100

    Test 3

--- Common/LX-Common.lua
+++ Common/LX-Common.lua
@@ -1,1 +1,9 @@
---Hoho
+-->Version=0.01<
+
+function Download_Needed(name,verion)
+	--CommonCheck
+	local Response = GetWebResult("raw.github.com", "/ChrisDujmic/BoL/master/Common/LX-Common.lua?rand="..math.random(1,10000))
+	local OnlineVersion = Response:match(text, "-->Version=(.*)<")
+	print(OnlineVersion)
+	return true
+end
