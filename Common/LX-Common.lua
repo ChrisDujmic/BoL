60c31c765f0caa01c213603b937bda6c2d00f237
ChrisDujmic <chrisdujmic@gmail.com>
2015-03-20 15:00:46 +0100

    Test 10

--- Common/LX-Common.lua
+++ Common/LX-Common.lua
@@ -1,21 +1,33 @@
---[Version0.02]
-local Common_Version = 0.02
+--[Version0.03]
+local Common_Version = 0.03
 
 function Update_Needed(name,version)
 	--CommonCheck
-	local Response = GetWebResult("raw.github.com", "/ChrisDujmic/BoL/master/Common/LX-Common.lua")
+	
+	local Updated = false
+	local Response = GetWebResult("raw.github.com", "/ChrisDujmic/BoL/master/Common/LX-Common.lua" .. "?rand="..math.random(1,10000))
 	local OnlineVersion = string.match(Response,"%[Version(.-)%]")
-	Chat_Print("LX-Common","(v" .. tostring(Common_Version) .." -> v" .. OnlineVersion ..")")
-	if (version < tonumber(OnlineVersion)) then
-		Common_Link = "https://raw.githubusercontent.com/ChrisDujmic/BoL/master/Common/LX-Common.lua" .."?rand="..math.random(1,10000)
-		
+	if (Common_Version < tonumber(OnlineVersion)) then
+		Common_Link = "https://raw.githubusercontent.com/ChrisDujmic/BoL/master/Common/LX-Common.lua" .."?rand="..math.random(1,10000)	
 		function AfterDownload()
-			Chat_Print("LX-Common","Updated (v" .. tostring(Common_Version) .." -> v" .. OnlineVersion ..") please Reload (double F9)")
+			Chat_Print("LX-Common","Updated LX-Common (v" .. tostring(Common_Version) .." -> v" .. OnlineVersion ..") please Reload (double F9)")
 		end
-		
 		DownloadFile(Common_Link, LIB_PATH .. "LX-Common.lua", AfterDownload)
 		return true
 	end
+	
+	Response = GetWebResult("raw.github.com", "/ChrisDujmic/BoL/master/" .. name .. ".lua" .. "?rand="..math.random(1,10000))
+	OnlineVersion = string.match(Response,"%[Version(.-)%]")
+	if (version < tonumber(OnlineVersion)) then
+		Script_Link = "https://raw.githubusercontent.com/ChrisDujmic/BoL/master/" .. name .. ".lua" .."?rand="..math.random(1,10000)
+		function AfterDownload()
+			Chat_Print(name,"Updated " .. name .." (v" .. tostring(version) .." -> v" .. OnlineVersion ..") please Reload (double F9)")
+		end
+		DownloadFile(Script_Link, SCRIPT_PATH .. name .. ".lua", AfterDownload)
+		return true
+	end
+	
+	return false
 end
 
 function Chat_Print(from,message)
