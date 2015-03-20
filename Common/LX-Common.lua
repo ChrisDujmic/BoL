581d7878e1528ac02e31d56752783312c6971f75
ChrisDujmic <chrisdujmic@gmail.com>
2015-03-20 14:01:20 +0100

    Test 7

--- Common/LX-Common.lua
+++ Common/LX-Common.lua
@@ -1,10 +1,22 @@
---[Version0.01]
+--[Version0.02]
+local Common_Version = 0.02
 
 function Update_Needed(name,version)
 	--CommonCheck
 	local Response = GetWebResult("raw.github.com", "/ChrisDujmic/BoL/master/Common/LX-Common.lua")
-	print(Response)
-	--local OnlineVersion = string.match(Response,"%[Version(.*)%]")
-	--print(OnlineVersion)
-	return true
+	local OnlineVersion = string.match(Response,"%[Version(.-)%]")
+	if (version < tonumber(OnlineVersion)) then
+		Common_Link = "https://raw.githubusercontent.com/ChrisDujmic/BoL/master/Common/LX-Common.lua" .."?rand="..math.random(1,10000)
+		
+		function AfterDownload()
+			Chat_Print("LX-Common","Common Updated (v " .. tostring(Common_Version) .." -> v " .. OnlineVersion ..") please Reload (double F9)")
+		end
+		
+		DownloadFile(Common_Link, LIB_PATH .. "LX-Common.lua", AfterDownload)
+		return true
+	end
+end
+
+function Chat_Print(from,message)
+print("<b><font color=\"#B0E57C\">" .. from .. ": </font><font color=\"#B4D8E7\">" .. message .. "</font></b>")
 end
